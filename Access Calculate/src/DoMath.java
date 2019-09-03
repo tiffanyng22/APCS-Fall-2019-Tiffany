b7714da24f705e291d43072bba61b6ed7916c734
APCS3rd <APCS3rd@MHS-LT9-16>
2019-09-03 11:51:52 -0700

    Part 0 done

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,13 @@
+/*Tiffany Ng
+ * Period 3
+ * August 29,2019
+ *Calls function from Calculate
+ */
+public class DoMath {
+	
+	public static void main(String[] args) {
+		System.out.println(Calculate.square(4));
+		System.out.println();
+		
+	}
+}
---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,26 @@
+/*Tiffany Ng
+ * August 29, 2019
+ * Library of math functions
+*/
+
+public class Calculate {
+	// a call to square returns the square of the value passed. The method accepts an integer and returns an integer.
+	public static int square(int number) {
+		int answer=0;
+		answer=number * number;
+		return answer;
+	}
+	//a call to to cube returns the cube of the value passed The method accepts an integer and returns an integer.
+	public static int cube(int number) {
+		int answer=0;
+		answer=number * number * number;
+		return answer;			
+    }
+	// a call to average returns the average of the values passed to it. The method accepts two doubles and returns a double.
+	public static int average(int number) {
+    	int answer=0;
+    	answer=number * number; 
+    	return answer;
+    }
+ }
+
